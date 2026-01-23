380af752cf0b2e46eeac6a619c53ee43c73e1e37
lucien95 <tanyilucien@gmail.com>
2026-01-22 21:12:19 -0500

    added and identity provider OpenID OIDC

--- infra/workload-identity.tf
+++ infra/workload-identity.tf
@@ -8,5 +8,25 @@
     display_name              = "GitHub Actions Pool"                                                                  
     description               = "Identity pool for GitHub Actions OIDC"                                                
     disabled                  = false                                                                                  
-  }                                                                                                                    
+  }           
+
+
+   resource "google_iam_workload_identity_pool_provider" "github_provider" {                           
+    workload_identity_pool_id          = google_iam_workload_identity_pool.github_pool.workload_identity_pool_id                             
+    workload_identity_pool_provider_id = "github-provider"                                            
+    display_name                       = "GitHub Actions Provider"                                    
+                                                                                                      
+    attribute_mapping = {                                                                             
+      "google.subject"       = "assertion.sub"                                                        
+      "attribute.actor"      = "assertion.actor"                                                      
+      "attribute.repository" = "assertion.repository"                                                 
+    }                                                                                                 
+                                                                                                      
+    # Only allow your specific repo                                                                   
+    attribute_condition = "assertion.repository == \"lucien95/Remote-vitals-streaming\""              
+                                                                                                      
+    oidc {                                                                                            
+      issuer_uri = "https://token.actions.githubusercontent.com"                                      
+    }                                                                                                 
+  }                                                                                                           
                                                                                                                        
