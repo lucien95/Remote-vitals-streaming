11a949d1070aa4dd3fca554a109864e91f28b398
lucien95 <tanyilucien@gmail.com>
2026-01-22 21:24:13 -0500

    Allow GitHub Actions to impersonate the terraform-ci service account

--- infra/workload-identity.tf
+++ infra/workload-identity.tf
@@ -28,5 +28,18 @@
     oidc {                                                                                            
       issuer_uri = "https://token.actions.githubusercontent.com"                                      
     }                                                                                                 
-  }                                                                                                           
+  }       
+
+   # Allow GitHub Actions to impersonate the terraform-ci service account                              
+  resource "google_service_account_iam_member" "github_actions_impersonate" {                         
+    service_account_id = "projects/${var.project_id}/serviceAccounts/terraform-ci@${var.project_id}.iam.gserviceaccount.com" 
+    role               = "roles/iam.workloadIdentityUser"                                             
+    member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_pool.name}/attribute.repository/lucien95/Remote-vitals-streaming"                                  
+  }                                                                                                   
+                                                                                                      
+  # Output the provider name (needed for GitHub Actions workflow)                                     
+  output "workload_identity_provider" {                                                               
+    description = "Workload Identity Provider for GitHub Actions"                                     
+    value       = google_iam_workload_identity_pool_provider.github_provider.name                     
+  }                                                                                                     
                                                                                                                        
